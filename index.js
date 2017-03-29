cc50aa4d9b9d30c1d6240d2a7b85c8b6a6d3cebe
Matt Kerchner <manafount@gmail.com>
2017-03-29 10:58:12 -0700

    messing around with events API

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const fetch = require('isomorphic-fetch');
 
-async function commitHistory(username) {
+async function getRepos(username) {
   let url = `https://api.github.com/users/${username}/repos`
-
+  console.log(`Getting repos for user ${username}...`)
   try {
     let response = await fetch(url);
     let repos = await response.json();
-    console.log(repos);
+    console.log(`Found ${repos.length} repos.`);
+    for (let i = 0; i < repos.length; i++) {
+      console.log(`Author of ${repos[i].name} is ${repos[i].owner.login}`);
+    }
   } catch (err) {
     console.log(err.message);
   }
-
 };
-commitHistory('test');
+
+async function getEvents(username) {
+  let url = `https://api.github.com/users/${username}/events/public`
+  console.log(`Getting events for user ${username}...`)
+  try {
+    let response = await fetch(url);
+    let events = await response.json();
+    console.log(`Found ${events.length} events.`);
+    for (let i = 0; i < events.length; i++) {
+      console.log(events[i]);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+// getRepos('test');
+getEvents('manafount');
